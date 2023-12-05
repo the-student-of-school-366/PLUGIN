8231356d541d53dac58e8d0fc12316137a33245c
GitHub Action <action@github.com>
2023-12-05 14:40:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/thestudentofschool366/plugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/thestudentofschool366/plugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.thestudentofschool366.plugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.thestudentofschool366.plugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
